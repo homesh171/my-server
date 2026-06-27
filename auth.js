b21c69bfcd19fd7bb509f3b66e1e7a7bfce52b60
Homesh <rameshnew586586@gmail.com>
2026-06-27 10:33:48 +0530

    first commit

---
+++ auth.js
@@ -0,0 +1,39 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const User = require('./User')
+
+const SECRET_KEY = 'mysecretkey123'
+
+async function signup(req, res) {
+  const { name, email, password } = req.body
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const newUser = new User({
+    name,
+    email,
+    password: hashedPassword
+  })
+
+  await newUser.save()
+  res.json({ message: 'User created successfully!' })
+}
+
+async function login(req, res) {
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+  if (!user) {
+    return res.status(400).json({ message: 'User not found!' })
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password)
+  if (!isMatch) {
+    return res.status(400).json({ message: 'Wrong password!' })
+  }
+
+  const token = jwt.sign({ id: user._id }, SECRET_KEY)
+  res.json({ token, name: user.name })
+}
+
+module.exports = { signup, login }
---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const User = require('./User')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb+srv://Homesh:Homesh123@cluster0.cx6z6ru.mongodb.net/?appName=Cluster0')
+  .then(() => console.log('MongoDB Connected!'))
+  .catch((err) => console.log(err))
+
+// GET all users
+app.get('/users', async (req, res) => {
+  const users = await User.find()
+  res.json(users)
+})
+
+// POST a new user
+app.post('/users', async (req, res) => {
+  const newUser = new User({
+    name: req.body.name,
+    email: req.body.email
+  })
+  await newUser.save()
+  res.json(newUser)
+})
+
+// DELETE a user
+app.delete('/users/:id', async (req, res) => {
+  await User.findByIdAndDelete(req.params.id)
+  res.json({ message: 'User deleted!' })
+})
+
+// UPDATE a user
+app.put('/users/:id', async (req, res) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name, email: req.body.email },
+    { new: true }
+  )
+  res.json(updatedUser)
+})
+
+const { signup, login } = require('./Auth')
+
+// add these routes:
+app.post('/signup', signup)
+app.post('/login', login)
+
+app.listen(5000, () => {
+  console.log('Server is running on port 5000!')
+})
