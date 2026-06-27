8d13e432af009a86d99abe9564ef640829abdf15
Homesh <rameshnew586586@gmail.com>
2026-06-27 10:57:37 +0530

    fix file name casing

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const User = require('./User')
+const User = require('./user')
 
 const app = express()
 
